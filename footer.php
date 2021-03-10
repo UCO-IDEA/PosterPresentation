611e976fcb35caf2b4351ed3beb44d5962b20998
zchishti <zchishti91@gmail.com>
2021-03-10 15:20:02 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <div class="col-12" id="footer">
-    <img class="footer_logo" src="../img/logo/UCO_White.svg">
-    <img class="footer_logo" src="../img/logo/IDEA_White.svg">
-    <img class="footer_logo_small" src="logo/Lumina_horiz_white.svg">
+    <img class="footer_logo" src="../img/logo/UCO_White.svg" alt="UCO">
+    <img class="footer_logo" src="../img/logo/IDEA_White.svg" alt="CeCE IDEA">
 </div>
