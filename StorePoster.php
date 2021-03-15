7cd7e8df2058fd329e237525a816316fac7ce256
Robert Wall <rwall3@uco.edu>
2021-03-15 14:57:38 -0500

    Fixed some newer PHP issues and requirements. updated google API location.

--- StorePoster.php
+++ StorePoster.php
@@ -1,38 +1,52 @@
 <?php
 
-require_once '../../Google/google-api-php-client-2.2.0/vendor/autoload.php';
+require_once '../../apis/google/google-api-php-client-2.4.0/vendor/autoload.php';
 
 require_once 'config.php';
 
 require_once 'DataAccess/PosterDataAccess.php';
 
 try {
-	$annotationArray = $_POST['annotations'];
-	
-	for($i = 1; $i <= count($annotationArray); $i++) {
-		$ann = $annotationArray[$i];
-		
-		$res = preg_match_all("%(?:(?:https?|ftp)://)((?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+))(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?%iu", 
-		$ann['content'], 
-		$matches,
-		PREG_PATTERN_ORDER);
-		
-		if ($res != false) {
-			if (array_search($matches[1][0], $config["allowedDomains"]) === false) {
-				$ann['content'] = str_replace($matches[0][0], "\"", $ann['content']);
-				
-				$annotationArray[$i] = $ann;
-			}
-		}
-	}
-	
-	$annotations = json_encode($annotationArray);
-	
-	$poster = putPosterData($annotations, $_POST['annotationStyle'], $_POST['annotationDOM'], $_POST['posterURL']);
-	
-	echo $poster;
-} catch (Exception $e) {
-	echo "-1";
-	echo $e;
+
+if(isset($annotationArray)){
+  $annotationArray = $_POST[ 'annotations' ];
+}else{
+    $annotationArray = "";
+}
+    
+    
+  $styleArray = $_POST[ 'annotationStyle' ];
+
+  $domArray = $_POST[ 'annotationDOM' ];
+
+
+  if ( !empty( $annotationArray ) ) {
+    for ( $i = 1; $i <= count( $annotationArray ); $i++ ) {
+      $ann = $annotationArray[ $i ];
+
+      $res = preg_match_all( "%(?:(?:https?|ftp)://)((?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+))(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?%iu",
+        $ann[ 'content' ],
+        $matches,
+        PREG_PATTERN_ORDER );
+
+      if ( $res != false ) {
+        if ( array_search( $matches[ 1 ][ 0 ], $config[ "allowedDomains" ] ) === false ) {
+          $ann[ 'content' ] = str_replace( $matches[ 0 ][ 0 ], "\"", $ann[ 'content' ] );
+
+          $annotationArray[ $i ] = $ann;
+        }
+      }
+    }
+  }
+
+  $annotations = json_encode( $annotationArray );
+
+  $poster = putPosterData( $annotations, $styleArray, $domArray, $_POST[ 'posterURL' ] );
+
+  echo $poster;
+
+} catch ( Exception $e ) {
+  echo "-1";
+  echo $e;
 }
 ?>
