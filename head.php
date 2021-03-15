f762489d110502957ec1a938e426f41889e1e7a6
Robert Wall <rwall3@uco.edu>
2021-03-15 14:56:19 -0500

    updated to use CDN

--- head.php
+++ head.php
@@ -2,9 +2,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
-<link rel="stylesheet" type="text/css" href="https://cece.uco.edu/idea/css/bootstrap-4.1.3.min.css">
-<!--<link href="https://www.uco.edu/uco-theme/_files/css/style.css?1507302961148" type="text/css" media="all" rel="stylesheet">-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-<script src="https://cece.uco.edu/idea/js/jquery-3.4.1.min.js"></script>
-<script src="https://cece.uco.edu/idea/js/popper.min.js"></script>
-<script src="https://cece.uco.edu/idea/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
